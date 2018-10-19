775b093ab0e3f0a202b1635dfec5c6925f9f5077
tomfriwel <976932447@qq.com>
2018-10-19 14:53:45 +0800

    a

--- prototype-pattern/prototype-pattern.js
+++ prototype-pattern/prototype-pattern.js
@@ -3,48 +3,49 @@ class Shape {
         if (new.target === Shape) {
             throw new Error('本类不能实例化');
         }
+        this.id = null
+        this.type = null
     }
-    draw() { }
+    getType() { }
+    getId() { }
+    setId() { }
+    clone() {
+        return Object.assign(Object.create(Object.getPrototypeOf(orig)), orig)
+    }
 }
 
 class Rectangle extends Shape {
+    constructor() {
+        super()
+        this.type = 'Rectangle'
+    }
     draw() {
         console.log('draw:' + this.constructor.name)
     }
 }
 
 class Square extends Shape {
+    constructor() {
+        super()
+        this.type = 'Square'
+    }
     draw() {
         console.log('draw:' + this.constructor.name)
     }
 }
 
 class Circle extends Shape {
+    constructor() {
+        super()
+        this.type = 'Circle'
+    }
     draw() {
         console.log('draw:' + this.constructor.name)
     }
 }
 
-class ShapeFactory {
-    getShape(shapeType) {
-        if (!shapeType) {
-            return null
-        }
-        if (shapeType.toLowerCase() == 'rectangle') {
-            return new Rectangle()
-        } else if (shapeType.toLowerCase() == 'square') {
-            return new Square()
-        } else if (shapeType.toLowerCase() == 'circle') {
-            return new Circle()
-        }
-        return null
-    }
-}
-let shapeFactory = new ShapeFactory()
-let shape1 = shapeFactory.getShape('rectangle')
-let shape2 = shapeFactory.getShape('square')
-let shape3 = shapeFactory.getShape('circle')
+class ShapeCache {
+    static getShape(shapeId) { }
 
-shape1.draw()
-shape2.draw()
-shape3.draw()
+    static loadCache() { }
+}
